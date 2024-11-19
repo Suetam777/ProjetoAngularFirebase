634c24a94146e6e65bfbc7356ddac19a3f5b4d3f
GabrielGCSOARES <126501271+GabrielGCSOARES@users.noreply.github.com>
2024-11-19 21:08:04 +0000

    asasasas

---
+++ src/app/shared/components/cabecalho/cabecalho.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CabecalhoComponent } from '../cabecalho/cabecalho.component';
+import { IonicModule } from '@ionic/angular';
+
+
+
+@NgModule({
+  declarations: [
+    CabecalhoComponent
+  ],
+  imports: [
+    CommonModule,
+    IonicModule
+  ],
+  exports: [
+    CabecalhoComponent
+  ]
+})
+export class ModuleModule { }
