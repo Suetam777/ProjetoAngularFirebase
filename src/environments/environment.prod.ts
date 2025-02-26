157b6459b60cae5cdbcad0aef4d496609c7d44c5
Mateus Azevedo <97805317+Suetam777@users.noreply.github.com>
2025-02-26 19:54:44 +0000

    conexão com banco de dados firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebaseConfig: {
-    apiKey: "AIzaSyCGdwCWx_8o2hYEmLBhK6xQzCMp5h12gzk",
-    authDomain: "aula-3e82a.firebaseapp.com",
-    projectId: "aula-3e82a",
-    storageBucket: "aula-3e82a.firebasestorage.app",
-    messagingSenderId: "258284807710",
-    appId: "1:258284807710:web:72b06b67cea8d15cd6b00f",
-    measurementId: "G-X8L8GS7897"
+    apiKey: "AIzaSyCbkSBWE8mpePlag9LQYmIKw59L6KuqUKs",
+    authDomain: "aula2025-24cad.firebaseapp.com",
+    projectId: "aula2025-24cad",
+    storageBucket: "aula2025-24cad.firebasestorage.app",
+    messagingSenderId: "810185359505",
+    appId: "1:810185359505:web:072737b4743b118f007f30",
+    measurementId: "G-RCLP3JX73M"
   },
   production: true
 };
